e882fdfe34672da869742421bb4d15df44f126ca
pazn1x <sainikunal636@gmail.com>
2024-04-26 14:21:35 +0530

    'rollback'

--- client/src/Useful/Useful.js
+++ client/src/Useful/Useful.js
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
  const Base_Api="http://localhost:5000/api" ;
+=======
+ const Base_Api="https://chat-mania-server.vercel.app/api" ;
+>>>>>>> parent of 85e3816 ('vercel_url')
 export default {Base_Api}
 
